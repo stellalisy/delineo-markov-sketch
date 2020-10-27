496f4008e391761f29e108b1faa2ee90a5255d05
Yixin Pan <ypan37@jhu.edu>
2020-10-27 03:48:23 -0400

    transition Matrix

---
+++ trans_mat.py
@@ -0,0 +1,41 @@
+import numpy as np
+import scipy.io as sio
+from scipy.special import softmax
+import pickle
+
+def transition_matrix():
+
+    #import data from hourly_data
+    hourly_data = []
+    with (open("hourly_data.pickle", "rb")) as openfile:
+        while True:
+            try:
+                hourly_data.append(pickle.load(openfile))
+            except EOFError:
+                break
+
+    trans_Ms = []
+    i = 0
+    #creating transition_matrix for every people
+    for people in hourly_data[0]:
+        trans_M = np.zeros((10000,10000))
+        previous_date = ""
+        previous_loc = -1
+        curr_date = ""
+        curr_loc = -1
+        for date in hourly_data[0].get(people):
+            curr_date = hourly_data[0].get(people).get(date)[0]
+            if curr_date[:11] == previous_date[:11]:
+                curr_loc = hourly_data[0].get(people).get(date)[2]
+                trans_M[previous_loc, curr_loc] = trans_M[previous_loc, curr_loc] + 1
+                previous_loc = curr_loc
+            else:
+                previous_loc = curr_loc
+                previous_date = curr_date
+        trans_M = softmax(trans_M, axis = 1)
+        trans_Ms.append(trans_M)
+        i+=1
+        print("Person " + str(i) + " matrix Created")
+    print(trans_Ms)
+if __name__ == '__main__':
+    transition_matrix()
